9379cacade018924ae3ef4ba9c08ea9cc3288d54
= <=>
2025-07-04 21:06:50 +0530

    Adding capital C to the component

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>Parent component</h1>
+        <h1>Parent Component</h1>
         <Login isLoggedIn={isLoggedin} setIsLoggedIn={setIsLoggedIn}/>
     </div>
   )
